646bf6e7ca92c1abda8dac11633982bbf30bdb52
TOPGamez <68175746+TOPGamez@users.noreply.github.com>
2020-11-01 17:40:29 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log("TOPBot is online!");
+});
+
+client.on('message' , message =>{
+    
+    message.member.roles.cache.has
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+   
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
